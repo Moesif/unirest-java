2ce24835b459b03a81715b552284fa56fdd8f1a6
Zeeshan Ejaz Bhatti <zeeshanejaz@gmail.com>
2015-02-05 17:39:21 +1300

    added a flat headers method

--- src/main/java/com/mashape/unirest/http/Headers.java
+++ src/main/java/com/mashape/unirest/http/Headers.java
@@ -1,7 +1,9 @@
 package com.mashape.unirest.http;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class Headers extends HashMap<String, List<String>> {
 
@@ -15,4 +17,15 @@ public class Headers extends HashMap<String, List<String>> {
 		return null;
 	}
 	
+	public Map<String, String> getFlatHeaders(){
+		Map<String, String> result = new HashMap<String, String>();
+		
+		for(Iterator<String> i = this.keySet().iterator(); i.hasNext();) {
+		    String key = i.next();
+		    String value = getFirst(key);
+		    result.put(key, value);
+		}
+		
+		return result;
+	}	
 }
